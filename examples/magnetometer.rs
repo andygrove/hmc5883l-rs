3bee50ce56bfd793380e7bfc522f23161634bb7f
Andy Grove <andygrove73@gmail.com>
2016-09-18 20:00:39 +0000

    1.0.1

--- examples/magnetometer.rs
+++ examples/magnetometer.rs
@@ -8,10 +8,18 @@ fn main() {
 
     let mut mag = HMC5883L::new("/dev/i2c-1", 0x1E).unwrap();
 
+    let gauss_lsb_xy = 1100.0;
+    let gauss_lsb_z  =  980.0;
+    let declination_angle = 0.22; // you need to set this based on your location
+
     loop {
         let (x, y, z) = mag.read().unwrap();
+
+        // convert to micro-teslas
+        let (x, y, z) = (x/gauss_lsb_xy*100.0, y/gauss_lsb_xy*100.0, z/gauss_lsb_z*100.0);
+
         println!("x={}, y={}, z={}", x, y, z);
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(500));
     }
 
 }
